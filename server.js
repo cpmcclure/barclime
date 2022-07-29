b11d507953efae4516b13045cb224e56d5071ea2
CP McClure <c.patrick.mcclure@gmail.com>
2022-07-29 18:55:12 -0400

    Add ejs rendering

--- server.js
+++ server.js
@@ -4,42 +4,48 @@ const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
-const PORT = 3000
+const PORT = 3001
 
 // const cors = require('cors')
 // app.use(cors)
 
 // Use libraries / middleware
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}))
+// app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+app.use(express.json())
+app.set('view engine', 'ejs')
 
 let db
 let dbConnectionStr = process.env.DB_STRING
 let dbName = 'barclime'
-let collection
 
 // Set up database
-MongoClient.connect(dbConnectionStr)
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
-        console.log(`Connected to ${dbName} Database`)
+        console.log(`Connected to ${dbName} database`)
         // assign db to connected db for later use
         db = client.db(dbName)
-        collection = db.collection('shot-data')
+        // shotData = db.collection('shot-data')
+        // shotData = db.collection("shot-data").findOne({}, function(err, result) {
+        //     if (err) throw err;
+        //     console.log(result)
+        //   });
     })
-    .catch(err => console.error(err))
 
 // Get  
-app.get('/', (req, res) => {
-    const mostRecent = collection.find().sort({"currentTime":1}).limit(1)
-    console.log(mostRecent)
-    res.sendFile(__dirname + '/index.html')
+app.get('/', async(request, response)=>{
+    let shotData = await db.collection("shot-data").findOne();
+    response.render('index.ejs', { lastShot: shotData })
+    // res.sendFile(__dirname + '/index.html')
 })
 
 // Post
 app.post('/add', (req, res) => {
-    collection.insertOne(req.body)
+    db.collection('shot-data').insertOne(req.body)
     .then(result => {
+        console.log(req.body)
         res.redirect('/')
     })
     .catch(error => console.error(error))
@@ -47,7 +53,7 @@ app.post('/add', (req, res) => {
 
 //Server
 app.listen(process.env.PORT || PORT, function(){
-    console.log('listening on 3000')
+    console.log(`listening on ${PORT}`)
 })
     
 
