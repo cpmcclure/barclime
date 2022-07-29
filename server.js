ab00e8f66dd47ec154b6f29deb4a455d9c357f30
CP McClure <c.patrick.mcclure@gmail.com>
2022-07-29 15:37:21 -0400

    Add .env file, update connection to database to self-contained function

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
+
+const PORT = 3000
 
 // const cors = require('cors')
 // app.use(cors)
@@ -11,32 +14,38 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))
 
+let db
+let dbConnectionStr = process.env.DB_STRING
+let dbName = 'barclime'
+let collection
+
 // Set up database
-const connectionString = 'mongodb+srv://cpmcclure:lR3divMAHDlChZ1f@cluster0.t1avrfg.mongodb.net/?retryWrites=true&w=majority'
-MongoClient.connect(connectionString)
+MongoClient.connect(dbConnectionStr)
     .then(client => {
-        console.log('Connected to Database')
-        const db = client.db('barclime')
-        const shotData = db.collection('shot-data')
-
-        // Get  
-        app.get('/', (req, res) => {
-            res.sendFile(__dirname + '/index.html')
-        })
-
-        // Post
-        app.post('/add', (req, res) => {
-            shotData.insertOne(req.body)
-            .then(result => {
-                res.redirect('/')
-            })
-            .catch(error => console.error(error))
-        })
-
-        //Server
-        app.listen(3000, function(){
-            console.log('listening on 3000')
-        })
+        console.log(`Connected to ${dbName} Database`)
+        // assign db to connected db for later use
+        db = client.db(dbName)
+        collection = db.collection('shot-data')
     })
     .catch(err => console.error(err))
+
+// Get  
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+// Post
+app.post('/add', (req, res) => {
+    collection.insertOne(req.body)
+    .then(result => {
+        res.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
+
+//Server
+app.listen(process.env.PORT || PORT, function(){
+    console.log('listening on 3000')
+})
+    
 
