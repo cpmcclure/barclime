09799ce15ea93c2838155fd96638c6311b2007e1
CP McClure <c.patrick.mcclure@gmail.com>
2023-07-08 21:12:13 -0400

    Simplify routes/profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const shotController = require('../controllers/shot') 
 const profileController = require('../controllers/profile')
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
