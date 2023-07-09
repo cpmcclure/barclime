bde161440f820dff963b3489b15c817fd48a5298
CP McClure <c.patrick.mcclure@gmail.com>
2023-07-08 21:09:16 -0400

    Simplify routes/main/js

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,6 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/auth') 
 const homeController = require('../controllers/home')
-const shotController = require('../controllers/shot') 
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 router.get('/', homeController.getIndex)
